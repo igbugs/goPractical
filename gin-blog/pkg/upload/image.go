f1565b0a1498e73b640723a543410eab92dd2af6
xue_yongbo@ <xue_yongbo@163.com>
2018-10-19 09:38:55 +0800

    image upload

---
+++ gin-blog/pkg/upload/image.go
@@ -0,0 +1,74 @@
+package upload
+
+import (
+	"gin-blog/pkg/setting"
+	"path"
+	"strings"
+	"gin-blog/pkg/util"
+	"gin-blog/pkg/file"
+	"mime/multipart"
+	"log"
+	"gin-blog/pkg/logging"
+	"os"
+	"fmt"
+)
+
+func GetImageFullUrl(name string) string {
+	return setting.AppSetting.ImagePrefixUrl + "/" + GetImagePath() + name
+}
+
+func GetImageName(name string) string {
+	ext := path.Ext(name)
+	fileName := strings.TrimSuffix(name, ext)
+	fileName = util.EncodeMD5(fileName)
+
+	return fileName + ext
+}
+
+func GetImagePath() string {
+	return setting.AppSetting.ImageSavePath
+}
+
+func GetImageFullPath() string {
+	return setting.AppSetting.RuntimeRootPath + GetImagePath()
+}
+
+func CheckImageExt(fileName string) bool {
+	ext := file.GetExt(fileName)
+	for _, allowExt := range setting.AppSetting.ImageAllowExts {
+		if strings.ToUpper(allowExt) == strings.ToUpper(ext) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckImageSize(f multipart.File) bool {
+	size, err := file.GetSize(f)
+	if err != nil {
+		log.Println(err)
+		logging.Warn(err)
+		return false
+	}
+
+	return size <= setting.AppSetting.ImageMaxSize
+}
+
+func CheckImage(src string) error {
+	dir, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("os.Getwd err: %v", err)
+	}
+
+	err = file.IsNotExistMkDir(dir + "/" + src)
+	if err != nil {
+		return fmt.Errorf("file.IsNotExistMkDir err: %v", err)
+	}
+
+	if file.CheckPermission(src) {
+		return fmt.Errorf("file.CheckPermission Permission ")
+	}
+
+	return nil
+}
--- gin-blog/routers/router.go
+++ gin-blog/routers/router.go
@@ -6,6 +6,8 @@ import (
 	"gin-blog/routers/api/v1"
 	"gin-blog/routers/api"
 	"gin-blog/middleware/jwt"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func InitRouter() *gin.Engine {
@@ -17,6 +19,8 @@ func InitRouter() *gin.Engine {
 	gin.SetMode(setting.ServerSetting.RunMode)
 
 	r.GET("/auth", api.GetAuth)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.POST("/upload", api.UploadImage)
 
 	apiv1 := r.Group("/api/v1")
 	apiv1.Use(jwt.JWT())
