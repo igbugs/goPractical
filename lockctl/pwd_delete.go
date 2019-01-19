9a3fb490d743b1fbafdcca41b7ea7fc1b225c744
xue_yongbo <xue_yongbo@163.com>
2019-01-20 04:58:30 +0800

    clear code

--- lockctl/pwd_delete.go
+++ lockctl/pwd_delete.go
@@ -21,48 +21,7 @@ type PwdDeleteRespData struct {
 type PwdDeleteResp struct {
 	Data PwdDeleteRespData `json:"data"`
 	RequestStatus
-	//RltCode string            `json:"rlt_code"`
-	//RltMsg  string            `json:"rlt_msg"`
 }
-
-//func PwdDelete(ctx *cli.Context, token string, pr *PwdDeleteReq) (rlt *PwdDeleteResp, err error) {
-//	logging.Debug("PwdDelete input parameter(pr): %#v", pr)
-//	data, _ := json.Marshal(pr)
-//	req, err := http.NewRequest("POST",
-//		"http://"+ctx.String("host")+"/pwd/delete",
-//		bytes.NewReader(data))
-//	if err != nil {
-//		logging.Error("NewRequest err: %v", err)
-//		return nil, err
-//	}
-//	req.Header.Set("version", "1.1")
-//	req.Header.Set("s_id", sid.String())
-//	req.Header.Set("access_token", token)
-//	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
-//
-//	resp, err := client.Do(req)
-//	if err != nil {
-//		logging.Error("client.Do err: %v", err)
-//		return nil, err
-//	}
-//	defer resp.Body.Close()
-//
-//	body, err := ioutil.ReadAll(resp.Body)
-//	if err != nil {
-//		logging.Error("read resp.Body err: %v", err)
-//		return nil, err
-//	}
-//	//fmt.Printf(string(body))
-//
-//	rlt = &PwdDeleteResp{}
-//	err = json.Unmarshal(body, rlt)
-//	if err != nil {
-//		logging.Error("unmarshal err: %v", err)
-//		return nil, err
-//	}
-//
-//	return rlt, nil
-//}
 
 func PwdDelete(ctx *cli.Context, token string, pr *PwdDeleteReq) (rlt *PwdDeleteResp, err error) {
 	logging.Debug("PwdDelete input parameter(pr): %#v", pr)
--- lockctl/pwd_list.go
+++ lockctl/pwd_list.go
@@ -14,8 +14,6 @@ type PwdLsReq struct {
 type PwdLsResp struct {
 	Data []*PwdLsRespData `json:"data"`
 	RequestStatus
-	//RltCode string           `json:"rlt_code"`
-	//RltMsg  string           `json:"rlt_msg"`
 }
 
 type PwdLsRespData struct {
@@ -29,45 +27,6 @@ type PwdLsRespData struct {
 	PwdUserMobile  string `json:"pwd_user_mobile"`
 	PwdUserIdcard  string `json:"pwd_user_idcard"`
 }
-
-//func PwdList(ctx *cli.Context, token string, pr *PwdLsReq) (rlt *PwdLsResp, err error) {
-//	logging.Debug("PwdList input parameter(pr): %#v", pr)
-//	data, _ := json.Marshal(pr)
-//	req, err := http.NewRequest("POST",
-//		"http://"+ctx.String("host")+"/pwd/list",
-//		bytes.NewReader(data))
-//	if err != nil {
-//		logging.Error("NewRequest err: %v", err)
-//		return nil, err
-//	}
-//	req.Header.Set("version", "1.1")
-//	req.Header.Set("s_id", sid.String())
-//	req.Header.Set("access_token", token)
-//	req.Header.Set("Content-Type", "application/json;charset=UTF-8")
-//
-//	resp, err := client.Do(req)
-//	if err != nil {
-//		logging.Error("client.Do err: %v", err)
-//		return nil, err
-//	}
-//	defer resp.Body.Close()
-//
-//	body, err := ioutil.ReadAll(resp.Body)
-//	if err != nil {
-//		logging.Error("read resp.Body err: %v", err)
-//		return nil, err
-//	}
-//	logging.Debug("get PwdList response body: %#v", string(body))
-//
-//	rlt = &PwdLsResp{}
-//	err = json.Unmarshal(body, rlt)
-//	if err != nil {
-//		logging.Error("unmarshal err: %v", err)
-//		return nil, err
-//	}
-//
-//	return rlt, nil
-//}
 
 func PwdList(ctx *cli.Context, token string, pr *PwdLsReq) (rlt *PwdLsResp, err error) {
 	logging.Debug("PwdList input parameter(pr): %#v", pr)
